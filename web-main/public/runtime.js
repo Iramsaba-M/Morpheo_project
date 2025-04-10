939b1204f6742c11e71e84e5d19a674f263159d1
Iramsaba-M <mirjankar.iramsaba@infokalash.com>
2025-04-10 22:12:59 +0530

    error resolved

--- web-main/public/runtime.js
+++ web-main/public/runtime.js
@@ -3,5 +3,5 @@ window.RUNTIME_ENV = {
   REACT_APP_AUTH0_CLIENT_ID: "IRizLtmah7nH1jGy3ZUdrP14eXlK56sQ",
   REACT_APP_AUTH0_CALLBACK_URL: "http://localhost:5173/callback",
   REACT_APP_AUTH0_LOGOUT_URL: "http://localhost:5173",
-  API_BASE_URL: "http://192.168.4.74:8080",
+  API_BASE_URL: "http://localhost:8080",
 };
